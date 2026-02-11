bb0fc55794790e0f80acd1784d19c8c5924aa9bc
Aditya <adityakanoujia30@gmail.com>
2026-02-12 01:46:58 +0530

    Added auth controller, service and DTO

---
+++ src/main/java/com/smartwallet/dto/LoginDto.java
@@ -0,0 +1,16 @@
+package com.smartwallet.dto;
+
+public class LoginDto {
+
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/com/smartwallet/dto/RegisterDto.java
@@ -0,0 +1,20 @@
+package com.smartwallet.dto;
+
+public class RegisterDto {
+
+    private String name;
+    private String email;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
